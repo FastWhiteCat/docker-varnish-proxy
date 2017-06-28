dfbbacaa115c6a669725a829ae35b5747c064bd4
Maciej <maciej.wieczorek@fastwhitecat.com>
2017-06-28 15:12:48 +0200

    Changes in rules

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ if [ -z "$ELB_DNS" ]; then
     ELB_DNS=""
 fi
 
-sed -i 's=__listen_port__='"$LISTEN_PORT"'=g' /etc/nginx/nginx.conf
-sed -i 's=__elb_dns__='"$ELB_DNS"'=g' /etc/nginx/nginx.conf
+sed -i 's=__listen_port__='"$LISTEN_PORT"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__elb_dns__='"$ELB_DNS"'=g' /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
