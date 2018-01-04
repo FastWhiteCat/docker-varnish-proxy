23c514a57fe82cac36a61a58fb2c2804ece59354
Adam Hyski <adam.hyski@fastwhitecat.com>
2018-01-04 12:18:27 +0100

    Basic wersion of dockre varnish proxy

--- start.sh
+++ start.sh
@@ -8,30 +8,7 @@ if [ -z "$ELB_DNS" ]; then
     ELB_DNS=""
 fi
 
-if [ -z "$SITEMAP_INDEX" ]; then
-    SITEMAP_INDEX="sitemap_index.xml"
-fi
-
-if [ -z "$SITEMAP_CATEGORY" ]; then
-    SITEMAP_CATEGORY="sitemap_category.xml"
-fi
-
-if [ -z "$SITEMAP_PRODUCTS" ]; then
-    SITEMAP_PRODUCTS="sitemap_products.xml"
-fi
-
-if [ -z "$SITEMAP_BLOG" ]; then
-    SITEMAP_BLOG="blog_sitemap.xml"
-fi
-
 sed -i 's=__listen_port__='"$LISTEN_PORT"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__elb_dns__='"$ELB_DNS"'=g' /etc/nginx/conf.d/default.conf
-
-sed -i 's=__sitemap_index__='"$SITEMAP_INDEX"'=g' /etc/nginx/conf.d/default.conf
-sed -i 's=__sitemap_category__='"$SITEMAP_CATEGORY"'=g' /etc/nginx/conf.d/default.conf
-sed -i 's=__sitemap_products__='"$SITEMAP_PRODUCTS"'=g' /etc/nginx/conf.d/default.conf
-sed -i 's=__sitemap_blog__='"$SITEMAP_BLOG"'=g' /etc/nginx/conf.d/default.conf
-sed -i 's=__sitemap_url__='"$SITEMAP_URL"'=g' /etc/nginx/conf.d/default.conf
-sed -i 's=__sitemap_blog_url__='"$SITEMAP_BLOG_URL"'=g' /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
