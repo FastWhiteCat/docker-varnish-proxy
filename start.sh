31d60624e7b4cc2d3c6fe958eb96876edb60f1ec
Maciej Wieczorek <maciej.wieczorek@fastwhitecat.com>
2018-11-07 12:24:56 +0100

    Changed file where option are set

--- start.sh
+++ start.sh
@@ -53,7 +53,7 @@ sed -i 's=__sitemap_blog_url__='"$SITEMAP_BLOG_URL"'=g' /etc/nginx/conf.d/defaul
 sed -i 's=__nginx_keepalive_timeout__='"$NGINX_KEEPALIVE_TIMEOUT"'=g' /etc/nginx/nginx.conf
 sed -i 's=__nginx_worker_connections__='"$NGINX_WORKER_CONNECTIONS"'=g' /etc/nginx/nginx.conf
 
-sed -i 's=__resolver_ip__='"$RESOLVER_IP"'=g' /etc/nginx/nginx.conf
-sed -i 's=__resolver_time__='"$RESOLVER_TIME"'=g' /etc/nginx/nginx.conf
+sed -i 's=__resolver_ip__='"$RESOLVER_IP"'=g' /etc/nginx/default.conf
+sed -i 's=__resolver_time__='"$RESOLVER_TIME"'=g' /etc/nginx/default.conf
 
 nginx -g 'daemon off;'
